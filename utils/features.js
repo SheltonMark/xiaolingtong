3f5ec41676ca6ff7ce4e930758b4e5cd89702914
SheltonMark <954174686@qq.com>
2026-03-24 21:42:54 +0800

    feat(miniprogram): enable certification sms ui

--- utils/features.js
+++ utils/features.js
@@ -1,5 +1,4 @@
-module.exports = {
-  // Keep certification SMS hidden for the urgent release until Tencent SMS
-  // sign/template approvals are ready, then switch to true and republish.
-  certSmsVerificationEnabled: false
+﻿module.exports = {
+  // Tencent Cloud certification SMS is now configured and enabled.
+  certSmsVerificationEnabled: true
 }
